6bdfaf351940a9b6a582d11e99836135303f604b
SunriseFading <91896985+SunriseFading@users.noreply.github.com>
2022-08-31 23:35:59 +0000

    Add librewolf

---
+++ librewolf.sh
@@ -0,0 +1,6 @@
+# add librewolf repository
+sudo rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3
+sudo dnf config-manager --add-repo https://rpm.librewolf.net
+
+# install librewolf
+sudo dnf install --refresh librewolf
