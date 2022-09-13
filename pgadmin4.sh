743ed1a1983612959d53d7463928fb9e5e6049ca
SunriseFading <91896985+SunriseFading@users.noreply.github.com>
2022-09-13 11:01:51 +0000

    Update pgadmin4.sh

--- pgadmin4.sh
+++ pgadmin4.sh
@@ -2,4 +2,4 @@
 sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-repo-2-1.noarch.rpm
 
 # install pgadmin4
-sudo yum install pgadmin4-desktop
+sudo yum install -y pgadmin4-desktop
